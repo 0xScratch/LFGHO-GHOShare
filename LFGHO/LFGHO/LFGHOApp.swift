3c848f437270bc7fa43abbd3f153aa92d1d62951
acse-am9023 <artemiy.acse@gmail.com>
2024-01-19 15:05:10 +0000

    added xcode files

---
+++ LFGHO/LFGHO/LFGHOApp.swift
@@ -0,0 +1,17 @@
+//
+//  LFGHOApp.swift
+//  LFGHO
+//
+//  Created by Artemiy Malyshau on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LFGHOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

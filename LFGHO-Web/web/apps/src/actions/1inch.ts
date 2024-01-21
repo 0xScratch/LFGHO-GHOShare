e41ce8c5cc7a50f116ff296e578805002b7dcb87
Jeevan Jutla <jj@JJPC.local>
2024-01-21 09:11:57 +0000

    added older 1inch

---
+++ LFGHO-Web/web/apps/src/actions/1inch.ts
@@ -0,0 +1,51 @@
+import { Hex, encodeFunctionData } from 'viem';
+import { executeTransaction } from '../lib/execute-transaction';
+import { workflowStepSchema } from '../../schemas';
+import { z } from 'zod';
+
+export const transformWorkflow = (step: any, chainId: number) => {
+  // TODO
+  return {
+    type: 'MINT_NFT',
+    chainId: chainId,
+
+    receiver: '0x82d3270f19CD2629005136089df8aFD96ff248a4',
+    amount: 10000,
+  };
+};
+
+export const getCallData = async ({ chainId }): Promise<Hex> => {
+  const to = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'; 
+  const value = 0n;
+  const data = '0x68656c6c6f'; 
+
+  const callData = encodeFunctionData({
+    abi: [
+      {
+        inputs: [
+          { name: 'dest', type: 'address' },
+          { name: 'value', type: 'uint256' },
+          { name: 'func', type: 'bytes' },
+        ],
+        name: 'execute',
+        outputs: [],
+        stateMutability: 'nonpayable',
+        type: 'function',
+      },
+    ],
+    args: [to, value, data],
+  });
+  return callData;
+};
+
+export const execute = async (
+  workflowStep: z.infer<typeof workflowStepSchema>
+) => {
+  console.log('Start 1inch.... swap');
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  console.log('1inch.... swap complete');
+
+  return {
+    ok: true,
+  };
+};
